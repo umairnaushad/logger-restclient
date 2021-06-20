70b398e64e011921426d56a6d6047c718d8c38f4
Umair Naushad <Umair.Naushad@emaratech.ae>
2021-06-20 16:11:32 +0400

    Working on GUI

---
+++ window_app/source/WPF Application/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Author
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public DateTime DOB { get; set; }
+            public string BookTitle { get; set; }
+            public bool IsMVP { get; set; }
+            public ImageSource Photo { get; set; }
+        }
+        private List<Author> LoadCollectionData()
+        {
+            List<Author> authors = new List<Author>();
+            authors.Add(new Author()
+            {
+                Id = 101,
+                Name = "Mahesh Chand",
+                BookTitle = "Graphics Programming with GDI+",
+                DOB = new DateTime(1975, 2, 23),
+                IsMVP = false,
+                Photo = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory+ "images\\image1.png"))
+            });
+            /*
+            authors.Add(new Author()
+            {
+                Id = 201,
+                Name = "Mike Gold",
+                BookTitle = "Programming C#",
+                DOB = new DateTime(1982, 4, 12),
+                IsMVP = true
+            });
+
+            authors.Add(new Author()
+            {
+                Id = 244,
+                Name = "Mathew Cochran",
+                BookTitle = "LINQ in Vista",
+                DOB = new DateTime(1985, 9, 11),
+                IsMVP = true
+            });
+            */
+            return authors;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            dataGridView1.ItemsSource = LoadCollectionData();
+        }
+    }
+}
